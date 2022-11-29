8e4f52417328d8ce7e808bbcc4ea3f9da1abb1f5
Machinato <maxgritsenko666@gmail.com>
2022-11-30 01:04:02 +0200

    Init application

---
+++ Laba 2 1.5.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+
+void checkValidInput()
+{
+	if (cin.fail())
+		throw "Incorrect input";
+}
+
+void checkValidParam(int n)
+{
+	if (n <= 2)
+		throw "Incorrect param";
+}
+double calculate(double x, int n)
+{
+	double y;
+	if (x <= 0)//Расчет y если х<=0
+	{
+		y = 0;
+		for (int i = 2; i <= n - 1; i++) {
+			y += x / i;
+
+		}
+	}
+
+	else {//Расчет y если х>0
+		y = 1;
+		double e;
+		for (int i = 1; i <= n - 1; i++) {
+			e = 0;
+			for (int j = 0; j <= i; j++) {
+				e += i / (j + x);
+
+			}
+			y *= e;
+		}
+	}
+	return y;
+}
+
+
+int main()
+{
+	try {
+		setlocale(LC_ALL, "RUS");
+		int  n, a;
+		double  x, h, b;
+
+
+		cout << "Введите n>=3: n= ";//Вводим значения
+		cin >> n;
+		cout << "Введите x: x= ";
+		cin >> x;
+		cout << "Введите шаг h: h= ";
+		cin >> h;
+		cout << "Введите b, диапазона (x...b) = ";
+		cin >> b;
+		checkValidInput();
+		checkValidParam(n);
+
+		cout << "Хотите сохранить значение y ? \n1 - Да\n0-Нет\n"; //Запись результата (по желанию пользователя
+		cin >> a;
+		checkValidInput();
+
+		ofstream fout; //открытие потока на запись в файле
+		fout.open("test.txt", ofstream::app);// Создание файла
+
+
+
+
+		for (x; x <= b; x += h) {
+			double y = calculate(x, n);
+			cout << "x = " << x << "; y = " << y << endl;
+			if (a) {//Запись y в файл
+				fout << "y = " << y << endl;
+			}
+		}
+	}
+	catch (char* err) {
+		cout << err << endl;
+		return -1;
+	}
+	catch (...) {
+		cout << "Something is going wrong" << endl;
+		return -2;
+	}
+	return 0;
+}
+
+	
---
+++ UnitTest1.cpp
@@ -0,0 +1,72 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "C:\Users\mason\source\repos\Laba 2 1.5\Laba 2 1.5\Laba 2 1.5.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(CalculateTest)
+    {
+    public:
+        TEST_METHOD(Calculate_x_1_n_1_returned_0dot5)
+        {
+            int n = 2;
+            double x = 1;
+            double res = calculate(x, n);
+            Assert::AreEqual(1.5, res);
+        }
+    public:
+
+        TEST_METHOD(Calculate_x_m1_n_2_return_0)
+        {
+            int n = 2;
+            double x = -1;
+            double res = calculate(x, n);
+            Assert::AreEqual(0., res);
+        }
+    public:
+
+        TEST_METHOD(Calculate_x_0_n_3_return_0)
+        {
+            int n = 3;
+            double x = 0;
+            double res = calculate(x, n);
+            Assert::AreEqual(0., res);
+        }
+    };
+    TEST_CLASS(CheckValidParamTest){
+  public :
+    TEST_METHOD(CheckValidParam_n_m1_exceptionTrown){
+        int n = 0;
+        try
+        {
+          checkValidParam(n);
+          Assert::Fail();
+        }
+        catch (...)
+        {
+            Assert::IsTrue(true);
+        }
+      }
+
+  public:
+    TEST_METHOD(CheckValidParam_n_9_exceptionNotTrown)
+    {
+      int n = 3;
+      try
+      {
+        checkValidParam(n);
+        Assert::IsTrue(true);
+      }
+      catch (...)
+      {
+        Assert::Fail();
+      }
+    }
+  };
+
+}
+
+
+
+
